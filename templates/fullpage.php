e6ee62524e752ad48d173315bf9eb5cde63ca41b
Julien Zerbib <Julien Zerbib@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-20 16:08:33 +0000

    Version 1.4 Removing of feature of empty post type in permalink for fullpage post type.
    git-svn-id: https://plugins.svn.wordpress.org/wp-fullpage/tags/1.4@969079 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/fullpage.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying all fullpages
+ *
+ * This is the template that displays all fullpages by default.
+ * Please note that this is the WP Fullpage construct of fullpages and that
+ * other 'pages' on your WordPress site will use a different template.
+ */
+
+WPFP()->get_header(); ?>
+
+<div id="fullpage">
+
+	<?php
+
+		while ( have_posts() ) : the_post();
+
+			// Start the sections Loop.
+			WPFP()->get_sections( WPFP_Query()->fullpage->post_name );
+
+		endwhile;
+
+	?>
+
+</div><!-- #fullpage -->
+
+<?php
+
+$navigation = WPFP_Query()->fullpage->fullpage_options['navigation'];
+
+if( 'yes' === $navigation )
+	WPFP()->get_navigation( WPFP_Query()->fullpage->post_name );
+
+// WPFP()->get_sidebar();
+WPFP()->get_footer();
